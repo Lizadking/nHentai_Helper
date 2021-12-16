cd65f0887eecc45e9d314b8c89d82d1342304f8c
Lizadking <torrencebarbourjr04@gmail.com>
2021-12-16 16:50:25 -0500

    hmm

---
+++ mainsrc.js
@@ -0,0 +1,14 @@
+
+
+
+document.getElementById('submission').addEventListener('click',function()
+    {
+        let defaultUrl= "https://nhentai.net/g/"
+        let from = document.forms.nHentaiHelper;
+        let elm = from.elements.ntag;
+        let strElm = String(elm.value);
+        console.log(strElm);
+        let finalUrl = defaultUrl+=strElm+"/";
+        
+        chrome.tabs.create({url:finalUrl});
+    });
